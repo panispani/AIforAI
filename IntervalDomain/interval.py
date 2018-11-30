e9a8bf1dca3105b6457a508d525d49f26cd8e1be
panpan2 <p.panayiotou2@gmail.com>
2018-11-30 11:21:54 +0100

    Add relu on a single number

--- IntervalDomain/interval.py
+++ IntervalDomain/interval.py
@@ -1,3 +1,6 @@
 def relu(I1):
     (from1, to1) = I1
     return (max(from1,  0), max(to1, 0))
+
+def relu_number(n):
+    return max(0, n)
--- IntervalDomain/testinterval.py
+++ IntervalDomain/testinterval.py
@@ -4,12 +4,20 @@ import unittest
 class TestInterval(unittest.TestCase):
 
     def test_relu(self):
-        print("Test Interval relu")
+        print("\nTest Interval relu")
         self.assertEqual((1, 1), relu((1, 1)))
         self.assertEqual((0, 0), relu((0, 0)))
         self.assertEqual((0, 0), relu((-4, -1)))
         self.assertEqual((0, 2), relu((-4, 2)))
         self.assertEqual((10, 11), relu((10, 11)))
+        print("PASSED")
+
+    def test_relu_number(self):
+        print("\nTest relu of a single number")
+        self.assertEqual(0, relu_number(-5))
+        self.assertEqual(0, relu_number(0))
+        self.assertEqual(3, relu_number(3))
+        print("PASSED")
 
 if __name__ == '__main__':
     unittest.main()
