a7bab8e8d739eced6c0baad2d2fd9a0633c9c07a
panpan2 <p.panayiotou2@gmail.com>
2018-11-30 11:32:40 +0100

    Implement abstract transformer for substraction over intervals

--- IntervalDomain/interval.py
+++ IntervalDomain/interval.py
@@ -5,7 +5,12 @@ def relu(I1):
 def relu_number(n):
     return max(0, n)
 
-def addition(I1, I2):
+def add(I1, I2):
     (from1, to1) = I1
     (from2, to2) = I2
     return (from1 + from2,  to1 + to2)
+
+def sub(I1, I2):
+    (from1, to1) = I1
+    (from2, to2) = I2
+    return (from1 - max(from2, to2),  to1 - min(from2, to2))
--- IntervalDomain/testinterval.py
+++ IntervalDomain/testinterval.py
@@ -3,6 +3,13 @@ import unittest
 
 class TestInterval(unittest.TestCase):
 
+    def test_relu_number(self):
+        print("\nTest relu of a single number")
+        self.assertEqual(0, relu_number(-5))
+        self.assertEqual(0, relu_number(0))
+        self.assertEqual(3, relu_number(3))
+        print("PASSED")
+
     def test_relu(self):
         print("\nTest Interval relu")
         self.assertEqual((1, 1), relu((1, 1)))
@@ -12,17 +19,21 @@ class TestInterval(unittest.TestCase):
         self.assertEqual((10, 11), relu((10, 11)))
         print("PASSED")
 
-    def test_relu_number(self):
-        print("\nTest relu of a single number")
-        self.assertEqual(0, relu_number(-5))
-        self.assertEqual(0, relu_number(0))
-        self.assertEqual(3, relu_number(3))
+    def test_add(self):
+        print("\nTest Interval addition")
+        self.assertEqual((2, 4), add((0, 1), (2, 3)))
+        self.assertEqual((1, 4), add((-1, 1), (2, 3)))
         print("PASSED")
 
-    def test_addition(self):
-        print("\nTest Interval addition")
-        self.assertEqual((2, 4), addition((0, 1), (2, 3)))
-        self.assertEqual((1, 4), addition((-1, 1), (2, 3)))
+    def test_sub(self):
+        print("\nTest Interval substraction")
+        self.assertEqual((-3, -1), sub((0, 1), (2, 3)))
+        self.assertEqual((-4, -1), sub((-1, 1), (2, 3)))
+        self.assertEqual((-7, -4), sub((-4, -2), (2, 3)))
+        self.assertEqual((4, 7), sub((1, 2), (-5, -3)))
+        self.assertEqual((2, 7), sub((-1, 2), (-5, -3)))
+        self.assertEqual((-4, 7), sub((-1, 2), (-5, 3)))
+        self.assertEqual((-2, 7), sub((1, 2), (-5, 3)))
         print("PASSED")
 
 if __name__ == '__main__':
